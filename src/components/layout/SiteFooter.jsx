a980c66d55f8c0eab0e5442485519e3a07f8da3f
JUNHOCHOI0309 <skyho0309@naver.com>
2026-06-25 13:35:11 +0900

    chore - 기타 정보 추가

--- src/components/layout/SiteFooter.jsx
+++ src/components/layout/SiteFooter.jsx
@@ -10,6 +10,12 @@ export function SiteFooter() {
         <p className="site-footer__brand">MMKorea</p>
         <p className="site-footer__meta">{t("footer.meta")}</p>
         <p className="site-footer__meta">상호명 : 모델라인 컴퍼니(주)</p>
+        <p className="site-footer__meta">사업자 등록번호 : 188-88-01118</p>
+        <p className="site-footer__meta">대표자명 : 박응준</p>
+        <p className="site-footer__meta">
+          사업장 주소 : 서울특별시 강남구 강남대로160길 15, 2층 (신사동)
+        </p>
+        <p className="site-footer__meta">사업장 전화번호 : 02-379-2222</p>
         <p className="site-footer__meta">상점아이디(MID) : mmkore4r0w</p>
       </div>
       <div className="site-footer__links">
